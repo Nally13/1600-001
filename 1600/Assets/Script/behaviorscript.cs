f4ba9f6f6a0c31e88d5fa88a6e7475c448d418a9
unknown <10671859@CS514-17.ad.uvu.edu>
2016-01-20 08:09:23 -0700

    Changes

---
+++ 1600/Assets/Script/behaviorscript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class behaviorscript : MonoBehaviour 
+{
+	void Start ()
+	{
+	}
+
+	void Update () 
+	{
+		if(Input.GetKeyDown (KeyCode.R)) 
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+		}
+		if(Input.GetKeyDown (KeyCode.G)) 
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		if(Input.GetKeyDown (KeyCode.B)) 
+		{
+			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		}
+	}
+}
---
+++ 1600/Assets/Script/variablesandfunctions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablesandfunctions : MonoBehaviour 
+{
+	public int myInt=2;
+
+
+	void Start () 
+	{
+	}
+
+	void Update()
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
