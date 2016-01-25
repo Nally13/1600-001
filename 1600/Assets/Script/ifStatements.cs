d41a44feff9fad597c888cd1600bd17e9323f739
unknown <10671859@CS514-17.ad.uvu.edu>
2016-01-25 08:52:32 -0700

    Adding ifStatements script

--- 1600/Assets/Script/ifStatements.cs
+++ 1600/Assets/Script/ifStatements.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ifStatements : MonoBehaviour {
+public class ifStatements : MonoBehaviour 
+{
+	float coffeeTemperature = 85.0f;
+	float hotLimitTemperature = 70.0f;
+	float coldLimitTemperature = 40.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		coffeeTemperature -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest()
+	{
+		if(coffeeTemperature > hotLimitTemperature) {
+			Debug.Log("Coffee is too hot.");
+		}
+		else if(coffeeTemperature < coldLimitTemperature) 
+		{
+			Debug.Log("Coffee is too cold.");
+		}
+		else
+		{
+			Debug.Log("Coffee is just right.");
+		}
 	}
 }
