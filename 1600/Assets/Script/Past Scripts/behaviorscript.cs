a2492e7fda7cc11135688c13cad57cbb76367654
Natali Ovalle <ovallenatali@yahoo.com>
2016-02-22 08:15:47 -0700

    Adding Unity Project

---
+++ 1600/Assets/Script/Past Scripts/behaviorscript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class behaviorscript : MonoBehaviour {
+
+	void Update () 
+	{
+	
+		if (Input.GetKeyDown (KeyCode.R)) {
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+		}
+		if (Input.GetKeyDown (KeyCode.G)) {
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		if (Input.GetKeyDown (KeyCode.B)) {
+			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		}
+	
+	}
+}
---
+++ 1600/Assets/Script/Past Scripts/ifStatements.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour 
+{
+	public float cellphoneBatteryLife = 100.0f;
+	public float okBatteryLife = 60.0f;
+	public float lowBatteryLife = 20.0f;
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		cellphoneBatteryLife -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest()
+	{
+		if(cellphoneBatteryLife > okBatteryLife) {
+			Debug.Log("Battery life is great.");
+		}
+		else if(cellphoneBatteryLife < lowBatteryLife) 
+		{
+			Debug.Log("Battery life is low.");
+		}
+		else
+		{
+			Debug.Log("Battery life is OK.");
+		}
+	}
+}
