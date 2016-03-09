ec4ba1a4c8844d188466de7c0707bb48ae871758
Natali Ovalle <ovallenatali@yahoo.com>
2016-03-09 08:39:42 -0700

    Fixing Arrays and Adding Enumerations script.

--- 1600/Assets/Script/Arrays.cs
+++ 1600/Assets/Script/Arrays.cs
@@ -5,13 +5,26 @@ public class Arrays : MonoBehaviour
 {
 	public GameObject[] players;
 
-
-	void Start () 
+	void Start ()
 	{
 		players = GameObject.FindGameObjectsWithTag ("Player");
 
-		for (int i = 0; i < players.Length; i++) {
-			Debug.Log ("Player Number " + i + " is named " + players [i].name);
+		for (int i = 0; i < players.Length; i++) 
+		{
+			Debug.Log ("Player Number " +i+ " is named " + players [i].name);
 		}
 	}
 }
+
+/*int[] myIntArray = new int[5];
+
+	void Start ()
+	{
+		myIntArray [0] = 13;
+		myIntArray [1] = 75;
+		myIntArray [2] = 5;
+		myIntArray [3] = 913;
+		myIntArray [4] = 1000;
+		}
+	}
+}*/
