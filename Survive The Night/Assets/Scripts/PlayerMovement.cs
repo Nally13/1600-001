547a608491001cd271d64bb5b233bae22a79eedf
Natali Ovalle <ovallenatali@yahoo.com>
2016-04-11 08:49:50 -0600

    Changing PlayerMovement script.

--- Survive The Night/Assets/Scripts/PlayerMovement.cs
+++ Survive The Night/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,20 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public float movespeed;
-	private float maxspeed = 10f;
+	public float forwardSpeed = 5;
+	public float upForce = 5f;
 
-	private Vector3 input;
+	void FixedUpdate ()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (forwardSpeed, 0);
+		if (Input.GetButton ("Jump"))
+		{
+			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, upForce));
+		}
+	}
+}
+
+	/*private Vector3 input;
 
 
 	// Use this for initialization
@@ -22,4 +32,4 @@ public class PlayerMovement : MonoBehaviour {
 		}
 	
 	}
-}
+}*/
